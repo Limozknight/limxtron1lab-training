c1e12e0deafccad42bfcff4039166119eb19b6a5
Limozknight <12211759@mail.sustech.edu.cn>
2026-01-09 20:25:07 +0800

    increase step height, add stairs

--- exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/limx_pointfoot_env_cfg.py
+++ exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/limx_pointfoot_env_cfg.py
@@ -32,6 +32,10 @@ class PFBaseEnvCfg(PFEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
+        # [Task 4 Optimization] 提高摆动高度范围，适应20cm楼梯
+        # Increase swing height range to handle 20cm stairs
+        self.commands.gait_command.ranges.swing_height = (0.15, 0.25)
+
         self.scene.robot = POINTFOOT_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.scene.robot.init_state.joint_pos = {
             "abad_L_Joint": 0.0,
@@ -658,10 +662,21 @@ class PFTask2And3EnvCfg_PLAY(PFTask2And3EnvCfg):
 class PFUnifiedEnvCfg(PFTerrainTraversalEnvCfgV2):
     """
     [Task 2 + 3 + 4] 全能统一环境。
-    基于优化过的地形配置 (V2)，强制开启推力，并提高追踪精度要求。
+    包含：平地精准行走 + 强力抗推 + 复杂地形 (现在的版本加入了楼梯)。
     """
     def __post_init__(self):
         super().__post_init__()
+
+        # --- 0. [Task 4] 激活楼梯地形 ---
+        # 混合地形：波浪、粗糙、楼梯 (Waves, Rough, Stairs)
+        self.scene.terrain.terrain_generator = STAIRS_TERRAINS_CFG 
+        # 激活地形课程学习
+        self.curriculum.terrain_levels.func = mdp.terrain_levels_vel
+        self.curriculum.terrain_levels.params = {
+            "asset_cfg": SceneEntityCfg("robot"),
+            "step_size": -1.0,  # 只要不倒地就升级
+            "tracking_vel_threshold": 0.5, # 速度跟踪要求降低，优先通过
+        }
 
         # --- 1. 恢复被 V2 关闭的推力 (Task 3) ---
         # 在崎岖地形上被推非常危险，所以这里是顶级难度
@@ -686,13 +701,18 @@ class PFUnifiedEnvCfg(PFTerrainTraversalEnvCfgV2):
         # 即使在地形上，也要尽力走准
         self.rewards.rew_lin_vel_xy_precise.weight = 3.0 # 原 6.0/8.0
         
-        # --- 3. 强化稳定性 (Task 3) ---
-        # 相比 V2 (2.0) 提高，为了抗推
-        self.rewards.rew_base_stability.weight = 1.0 # [Fix] 不要太高，给移动容错空间
+        # --- 3. 强化稳定性 (Task 3 & 4) ---
+        # 爬楼梯需要极高的身体稳定性，否则会踩空
+        self.rewards.rew_base_stability.weight = 2.5 # [Upgrade] 1.0 -> 2.5 为了楼梯拼了
 
         # --- 4. 严厉惩罚 ---
         # [Fix] 之前是 -4.0，还是太高了，会导致机器人因为达不到完美高度而即使倒地也没区别
         self.rewards.pen_base_height.weight = -1.0
+        
+        # --- 5. [Task 4] 关键修改：强制抬腿 (Feet Regulation) ---
+        # 你的曲线显示这一项下降很厉害，这在楼梯上是致命的。
+        # 我们必须让它意识到乱踩是要命的。
+        self.rewards.pen_feet_regulation.weight = -1.0 # 之前是 -0.2，增强5倍惩罚，强制规范步伐
 
 
 @configclass
